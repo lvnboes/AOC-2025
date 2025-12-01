cff6619de50a6f97143f1c7c7742bb56f84448a6
lvnboes <lvn.boes@gmail.com>
2025-12-02 00:36:43 +0100

    much-better-and-simpler-solution-with-typing

--- bin/main.ml
+++ bin/main.ml
@@ -1,16 +1,20 @@
 open Aoc
 
 let day_01 =
-  let read_t, data = Timer.timed_excecution
-               (fun _ -> Reader.read_and_process_lines
-                           "data/d1.txt"
-                           Day01.parse_rotation)
+  let read_t, data =
+    Timer.timed_excecution (fun _ ->
+        Reader.read_and_process_lines "data/d1.txt" Day01.parse_rotation)
   in
   let p1_t, (res_1, res_2) =
-    Timer.timed_excecution (fun _ -> Day01.count_0 50 data) in
-    print_float read_t; print_newline ();
-    print_float p1_t; print_newline ();
-    print_int res_1; print_newline ();
-    print_int res_2; print_newline ()
+    Timer.timed_excecution (fun _ -> Day01.count_0 50 data)
+  in
+  print_float read_t;
+  print_newline ();
+  print_float p1_t;
+  print_newline ();
+  print_int res_1;
+  print_newline ();
+  print_int res_2;
+  print_newline ()
 
 let () = day_01
