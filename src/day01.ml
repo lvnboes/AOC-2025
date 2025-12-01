cc74a49d0de5bb0d9ea82c9ad9544e9d7b6fccc9
lvnboes <lvn.boes@gmail.com>
2025-12-02 00:27:38 +0100

    much-better-and-simpler-solution-with-typing

--- src/day01.ml
+++ src/day01.ml
@@ -1,17 +1,18 @@
-let parse_rotation (line: string): char * int =
-  Scanf.sscanf line "%c%d" (fun c d -> (c, d))
+type dir = L | R
 
-let execute_rotation (pos: int) ((dir, rot): (char * int)) : (int * int) =
+let parse_rotation (line: string): dir * int =
+  Scanf.sscanf line "%c%d" (fun c d -> (if c = 'L' then L else R), d)
+
+let execute_rotation (pos: int) ((dir, rot): (dir * int)) : (int * int) =
   let cross_0 old nw = if (old < 0 && nw >= 0) || (old > 0 && nw <= 0)
                        then 1 else 0
   in match dir with
-     | 'L' -> let new_pos = (pos - rot) in
+     | L -> let new_pos = (pos - rot) in
               new_pos mod 100, abs (new_pos / 100) + (cross_0 pos new_pos)
-     | 'R' -> let new_pos = (pos + rot) in
+     | R -> let new_pos = (pos + rot) in
               new_pos mod 100, new_pos / 100 + (cross_0 pos new_pos)
-     | _ -> failwith "invalid instruction"
 
-let count_0 (position: int) (instructions: (char * int) list) : (int * int) =
+let count_0 (position: int) (instructions: (dir * int) list) : (int * int) =
   let rec aux acc1 acc2 pos instr =
     match instr with
     | [] -> acc1, acc2
