a3b679ad59a4c5ba235a8b1e20870edff3b2cf38
lvnboes <lvn.boes@gmail.com>
2025-12-02 02:14:23 +0100

    add-pretty-printer

--- bin/main.ml
+++ bin/main.ml
@@ -5,16 +5,9 @@ let day_01 =
     Timer.timed_excecution (fun _ ->
         Reader.read_and_process_lines "data/d1.txt" Day01.parse_rotation)
   in
-  let p1_t, (res_1, res_2) =
+  let timed_result =
     Timer.timed_excecution (fun _ -> Day01.count_0 50 data)
   in
-  print_float read_t;
-  print_newline ();
-  print_float p1_t;
-  print_newline ();
-  print_int res_1;
-  print_newline ();
-  print_int res_2;
-  print_newline ()
+  Printer.print_day_combined 1 read_t timed_result
 
 let () = day_01
