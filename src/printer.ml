26a6b15d097babb8d049226e58149f16228a2b9c
lvnboes <lvn.boes@gmail.com>
2025-12-02 02:14:32 +0100

    add-pretty-printer

---
+++ src/printer.ml
@@ -0,0 +1,10 @@
+let print_day day read_time (time1, part1) (time2, part2) =
+  Printf.printf "Day %d\t| time\t\t| result\n" day;
+  Printf.printf "%s\n" (String.make 32 '-');
+  Printf.printf "Parsing\t| %.3fµs\t|\n" read_time;
+  Printf.printf "Part 1\t| %.3fµs\t| %d\n" time1 part1;
+  Printf.printf "Part 2\t| %.3fµs\t| %d\n" time2 part2;
+  print_newline ()
+
+let print_day_combined day read_time (solve_time, (part1, part2)) =
+  print_day day read_time (solve_time, part1) (0., part2)
