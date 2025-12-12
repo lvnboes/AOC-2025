0a8c61795253cf25ed32dde4c9fb4f7fdecb9c61
lvnboes <lvn.boes@gmail.com>
2025-12-12 02:25:14 +0100

    read-matrix

--- src/day04.ml
+++ src/day04.ml
@@ -6,11 +6,11 @@ let map_warehouse_line (line : string) =
   in
   line |> String.to_seq |> Seq.map map_location |> List.of_seq
 
-let padd (lst : 'a list) (padding : 'a) (f : 'a -> 'a) : 'a list =
+let padd_with (lst : 'a list) (padding : 'a) (f : 'a -> 'a) : 'a list =
   let rec aux = function [] -> [ padding ] | h :: t -> f h :: aux t in
   padding :: aux lst
 
 let padd_matrix (matrix : int list list) =
   let matrix_width = match matrix with [] -> 0 | h :: _ -> List.length h in
   let matrix_padding = List.init (matrix_width + 2) (fun _ -> 0) in
-  padd matrix matrix_padding (fun row -> padd row 0 (fun x -> x))
+  padd_with matrix matrix_padding (fun row -> padd_with row 0 (fun x -> x))
