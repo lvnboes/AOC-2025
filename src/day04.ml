3238c22068467754c3b2b39c02e9bfeb1f91c646
lvnboes <lvn.boes@gmail.com>
2025-12-14 00:59:19 +0100

    ugly-but-it-works

--- src/day04.ml
+++ src/day04.ml
@@ -1,19 +1,19 @@
 let map_warehouse_line (line : string) =
-  let map_location = function
+  line |> String.to_seq
+  |> Seq.map (function
     | '.' -> 0
     | '@' -> 1
-    | _ -> failwith "invalid character"
-  in
-  line |> String.to_seq |> Seq.map map_location |> List.of_seq
+    | _ -> invalid_arg "warehouse line")
+  |> List.of_seq
 
-let padd_with (lst : 'a list) (padding : 'a) (f : 'a -> 'a) : 'a list =
+let padd_map (lst : 'a list) (padding : 'a) (f : 'a -> 'a) : 'a list =
   let rec aux = function [] -> [ padding ] | h :: t -> f h :: aux t in
   padding :: aux lst
 
 let padd_matrix (matrix : int list list) =
   let matrix_width = match matrix with [] -> 0 | h :: _ -> List.length h in
   let matrix_padding = List.init (matrix_width + 2) (fun _ -> 0) in
-  padd_with matrix matrix_padding (fun row -> padd_with row 0 (fun x -> x))
+  padd_map matrix matrix_padding (fun row -> padd_map row 0 (fun x -> x))
 
 let clean_row (before : int list) (row : int list) (next : int list)
     (count : int) : int list * int =
