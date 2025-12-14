90a9b7be60d24519e5043aba7ffa5d39e51b6f11
lvnboes <lvn.boes@gmail.com>
2025-12-14 01:14:10 +0100

    ugly-but-it-works

--- src/debug.ml
+++
@@ -1,8 +0,0 @@
-let pp_int_list lst =
-  let rec print_elements = function
-    | [] -> ()
-    | h :: t -> print_int h; print_string "; "; print_elements t
-  in
-  print_string "[";
-  print_elements lst;
-  print_string "]";
